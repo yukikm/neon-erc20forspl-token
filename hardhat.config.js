5495e7fe5445ee136e6b4bad7efb9c8c16559271
yukikm <yuki.kimura.77777@gmail.com>
2025-05-19 20:18:49 +0900

    feat: add initial project setup with package.json and configuration scripts
    - Created package.json with dependencies for ERC20 and Solana integration.
    - Implemented TestDevBootcamp.js script for deploying and interacting with ERC20ForSPLMintable contracts on Solana.
    - Added config.js for managing Solana and EVM configurations, including utility functions for token handling and transaction preparation.

---
+++ hardhat.config.js
@@ -0,0 +1,61 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.26",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 10000,
+          },
+        },
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: {
+      neonevm: "test",
+    },
+    customChains: [
+      {
+        network: "neonevm",
+        chainId: 245022926,
+        urls: {
+          apiURL: "https://neon-devnet.blockscout.com/api",
+          browserURL: "https://neon-devnet.blockscout.com",
+        },
+      },
+      {
+        network: "neonevm",
+        chainId: 245022934,
+        urls: {
+          apiURL: "https://neon.blockscout.com/api",
+          browserURL: "https://neon.blockscout.com",
+        },
+      },
+    ],
+  },
+  networks: {
+    neondevnet: {
+      url: "https://devnet.neonevm.org",
+      accounts: [process.env.PRIVATE_KEY_OWNER, process.env.USER1_KEY],
+      chainId: 245022926,
+      allowUnlimitedContractSize: false,
+      gas: "auto",
+      gasPrice: "auto",
+    },
+    neonmainnet: {
+      url: "https://neon-proxy-mainnet.solana.p2p.org",
+      accounts: [process.env.PRIVATE_KEY_OWNER, process.env.USER1_KEY],
+      chainId: 245022934,
+      allowUnlimitedContractSize: false,
+      gas: "auto",
+      gasPrice: "auto",
+    },
+  },
+};
